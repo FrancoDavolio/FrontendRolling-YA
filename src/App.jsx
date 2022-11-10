1aca612db85b1c1d0f2a07427e2872de3a967bcb
francoDavolio <francodavolio12@gmail.com>
2022-11-10 19:58:43 -0300

    corregi unos errores en los nombres de los componentes crear y editar

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
         <Route exact path="/cardproducto" element={<CardProducto></CardProducto>}></Route>
         <Route exact path="/crearproducto" element={<CrearProducto></CrearProducto>}></Route>
         <Route exact path="/editar/:id" element={<EditarProducto></EditarProducto>}></Route>
-        <Route exact path="/itemproducto" element={<Error404></Error404>}></Route>
+        <Route exact path="/itemproducto" element={<ItemProducto></ItemProducto>}></Route>
       </Routes>
       <Footer></Footer>
     </BrowserRouter>
