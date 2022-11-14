a1e918fd28298b2db1c4c263d5bd068a0f7d19a7
francoDavolio <francodavolio12@gmail.com>
2022-11-14 11:52:05 -0300

    se protegieron las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,38 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "./components/common/Menu";
 import Footer from "./components/common/Footer";
-import Login from "./components/views/Login"
-import Register from "./components/views/Register"
-import Inicio from "./components/views/Inicio"
-import Administrador from "./components/views/Administrador"
-import AcercaDe from "./components/views/AcercaDe"
-import Detalle from "./components/views/Detalle"
-import Pedidos from "./components/views/Pedidos"
-import Error404 from "./components/views/Error404"
-import CardProducto from "./components/views/adminProductos/CardProducto"
-import CrearProducto from "./components/views/adminProductos/CrearProducto"
-import EditarProducto from "./components/views/adminProductos/EditarProducto"
-import ItemProducto from "./components/views/adminProductos/ItemProducto"
+import Login from "./components/views/Login";
+import Inicio from "./components/views/Inicio";
+import Detalle from "./components/views/Detalle";
+import Error404 from "./components/views/Error404";
+import { useState } from "react";
+import RutasProtegidas from "./components/routes/RutasProtegidas";
+import RutasAdmin from "./components/routes/RutasAdmin";
 
 function App() {
+  const usuario = JSON.parse(localStorage.getItem("tokenRollingYa")) || {};
+  const [usuarioLogueado, setUsuarioLogueado] = useState(usuario);
+
   return (
     <BrowserRouter>
       <Menu></Menu>
       <Routes>
-        <Route exact path="/menu" element={<Menu></Menu>}></Route>
-        <Route exact path="/footer" element={<Footer></Footer>}></Route>
         <Route exact path="/" element={<Inicio></Inicio>}></Route>
-        <Route exact path="/login" element={<Login></Login>}></Route>
-        <Route exact path="/register" element={<Register></Register>}></Route>
-        <Route exact path="/login" element={<Login></Login>}></Route>
-        <Route exact path="/administrador" element={<Administrador></Administrador>}></Route>
-        <Route exact path="/acercade" element={<AcercaDe></AcercaDe>}></Route>
         <Route exact path="/detalle/:id" element={<Detalle></Detalle>}></Route>
-        <Route exact path="/pedidos" element={<Pedidos></Pedidos>}></Route>
+        <Route
+          exact
+          path="/login"
+          element={<Login setUsuarioLogueado={setUsuarioLogueado}></Login>}
+        ></Route>
+        <Route
+          path="/adiministrador/*"
+          element={
+            <RutasProtegidas>
+              <RutasAdmin setUsuarioLogueado={setUsuarioLogueado}></RutasAdmin>
+            </RutasProtegidas>
+          }
+        ></Route>
         <Route exact path="*" element={<Error404></Error404>}></Route>
-        <Route exact path="/cardproducto" element={<CardProducto></CardProducto>}></Route>
-        <Route exact path="/crearproducto" element={<CrearProducto></CrearProducto>}></Route>
-        <Route exact path="/editar/:id" element={<EditarProducto></EditarProducto>}></Route>
-        <Route exact path="/itemproducto" element={<ItemProducto></ItemProducto>}></Route>
       </Routes>
       <Footer></Footer>
     </BrowserRouter>
---
+++ src/components/routes/RutasAdmin.jsx
@@ -0,0 +1,46 @@
+import { Route, Routes } from "react-router-dom";
+import Administrador from "../views/Administrador";
+import CrearProducto from "../views/adminProductos/CrearProducto";
+import EditarProducto from "../views/adminProductos/EditarProducto";
+import Register from "../views/Register";
+
+const RutasAdmin = ({ setUsuarioLogueado }) => {
+  return (
+    <div>
+      <>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={<Administrador></Administrador>}
+          ></Route>
+        </Routes>
+        <Routes>
+          <Route
+            exact
+            path="/crearproducto"
+            element={<CrearProducto></CrearProducto>}
+          ></Route>
+        </Routes>
+        <Routes>
+          <Route
+            exact
+            path="/editar/:id"
+            element={<EditarProducto></EditarProducto>}
+          ></Route>
+        </Routes>
+        <Routes>
+          <Route
+            exact
+            path="/register"
+            element={
+              <Register setUsuarioLogueado={setUsuarioLogueado}></Register>
+            }
+          ></Route>
+        </Routes>
+      </>
+    </div>
+  );
+};
+
+export default RutasAdmin;
