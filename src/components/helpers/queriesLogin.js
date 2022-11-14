2422f41f8027a2c52b2700f2d318abd9a67c7886
francoDavolio <francodavolio12@gmail.com>
2022-11-14 10:33:20 -0300

    cree la peticion post para cuando cree  los usuarios

--- src/components/helpers/queriesLogin.js
+++ src/components/helpers/queriesLogin.js
@@ -1,11 +1,24 @@
 const URL = process.env.REACT_APP_ROLLING_YA_USER
 
-export const consultarUserAPi = async ()=>{
-    try {
-        const respuesta=await fecht(URL)
-        const listaUsuarios = await respuesta.json()
-        return listaUsuarios
-    } catch (error) {
-        console.log(error)
-    }
+export const consultarUserAPi = async () => {
+  try {
+    const respuesta = await fecht(URL)
+    const listaUsuarios = await respuesta.json()
+    return listaUsuarios
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const crearUsuarioAPI = async (usuario) => {
+  try {
+    const respuesta = await fech(URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(usuario),
+    })
+    return respuesta
+  } catch (error) {
+    console.log(error)
+  }
 }
