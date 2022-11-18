2efac234822633b43f771bce6bfcbcc993074226
francoDavolio <francodavolio12@gmail.com>
2022-11-18 14:07:21 -0300

    puse el logo en el footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,8 +1,11 @@
-
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer className="negro text-center text-light py-3 ">
+      <Link to="/">
+        <img src="Logo_fondo_blanco" alt="logo de rolling ya" />
+      </Link>
       <p>
         <small>&copy; Todos los derechos reservados</small>
       </p>
