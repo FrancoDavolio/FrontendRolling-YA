338d8179a972bf039fa4ef5b9017d4fa9946679f
Leah <leahthomsen@gmail.com>
2022-11-19 21:05:41 -0300

    trialling some hamburger modals

---
+++ src/views/Detalle.jsx
@@ -0,0 +1,40 @@
+import { Container, Button, Card, Form} from "react-bootstrap";
+
+const Detalle = () => {
+  return(
+    <Container>
+      <section>
+        <h1 className="text-center">Hamburguesa Doble Queso</h1>
+        <p> className="<fs-3>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error doloremque beatae possimus deleniti voluptates quasi neque non, asperiores enim adipisci maiores autem alias quas ex! Porro illum possimus expedita nesciunt.
+        </p>
+      </section>
+      <section className="d-flex justify-content-be
+      tween">
+        <div className="col-4">
+<Card style={{ width: "25rem"}}>
+  <CardImg
+  variant="top"
+  src="https://images.pexels.com/photos/1199957/pexels-photo-1199957.jpeg?auto=compress&cs=tinysrgb&w=600"
+  alt="hamburguesa"
+  className="w-100"
+  />
+</Card>
+</div>
+<div className="col-6">
+  <h2 className="my-4">Precio: $1000</h2>
+  <Form.Check className="my-3 display-5" inline lavel="Carne" type={"checkbox"}></Form.Check>
+  <Form.Check className="my-3 display-5" inline lavel="Pan" type={"checkbox"}></Form.Check>
+  <Form.Check className="my-3 display-5" inline lavel="Carne" type={"checkbox"}></Form.Check>
+  <Form.Check className="my-3 display-5" inline lavel="Cebolla" type={"checkbox"}></Form.Check>
+  <Form.Check className="my-3 display-5" inline lavel="Carne" type={"checkbox"}></Form.Check>
+  <Form.Check className="my-3 display-5" inline lavel="Cheddar" type={"checkbox"}></Form.Check>
+  <Form.Check className="my-3 display-5" inline lavel="Bacon" type={"checkbox"}></Form.Check>
+</div>
+      </section>
+    </Container>
+  )
+}
+
+<title className="centred text"></title>
+<HTMLHeadingElement = "description"
