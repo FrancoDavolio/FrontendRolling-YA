0dfe2bdfd865ca36776a9cb3fce51a69db9a2de5
Leah <leahthomsen@gmail.com>
2022-11-23 15:35:23 -0300

    adding product info and images

--- src/components/views/ProductDetailPage/ProductDetailPage.jsx
+++ src/components/views/ProductDetailPage/ProductDetailPage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import Productimages, from 'section'
 
 function DetailProductPage(){
 
@@ -11,9 +12,29 @@ function DetailProductPage(){
     Axios.get('/api/product/products_by_id?id=${productId}&type=single')
    then (response => {
     setProduct(response.data[0])
-
-
    })
+
+},[])
+
+return (
+    <div className='postPage' style={{ width '100%, padding: '3rem 4rem'}}/>
+
+    <div style={{ display: 'flex  justifyContent: 'center'}}
+    <h1> product title</h1>
+    </div>
+    <br/>
+
+<Row gutter={[6, 16]} >
+    <Col lg={12} xs={24}>
+        <Productimage detail={Product} />
+    </Col>
+    <Col lg={12} xs={24}>
+        <Product info detail={Product} />
+    </Col>
+</Row>
+</div>
+)
+
     Axios.post('api/product/getProduct, variable')
     };
     }, [input])
---
+++ src/components/views/ProductDetailPage/sections/Product Images.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState }from 'react';
+import ImageGallery from 'react-image-gallery';
+
+function ProductImages(props) {
+
+    const [images, setImages] = useState()
+
+    useEffect(()) => {
+
+    }, [])
+    
+    if ( props.detail.images && props.detail.images.length > 0 {
+        let images = [];
+
+
+        props.detail.images && props.detail.images.map(item) => {
+            images.push({
+                original: 'http//localhost:5500/$(item)',
+                thumbnail 'http://localhost:5500/$(item)'
+            })
+        })
+       setImages(images) 
+        
+    }
+    
+    
+    return (
+        <div>
+
+        ImageGallery items={Images}/>
+        </div>
+    )
+}
+export default ProductImage
