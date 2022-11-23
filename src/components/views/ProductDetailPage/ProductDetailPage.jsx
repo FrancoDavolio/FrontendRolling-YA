921bd785ff45c50a7480b6b900cad2ae1bc10258
Leah <leahthomsen@gmail.com>
2022-11-23 14:29:26 -0300

    fetch info del producto del BD

--- src/components/views/ProductDetailPage/ProductDetailPage.jsx
+++ src/components/views/ProductDetailPage/ProductDetailPage.jsx
@@ -2,9 +2,18 @@ import React, { useEffect } from 'react'
 
 function DetailProductPage(){
 
+    const productId = Props.match.params.productId
+    const [Product, setProduct] = useState([])
+    const [state, setstate] = useState([intialState])
+
+
     useEffect() => {
-    //Axios.get('/api/product/products_by_id?id=${productId}&type=single')
-   
+    Axios.get('/api/product/products_by_id?id=${productId}&type=single')
+   then (response => {
+    setProduct(response.data[0])
+
+
+   })
     Axios.post('api/product/getProduct, variable')
     };
     }, [input])
