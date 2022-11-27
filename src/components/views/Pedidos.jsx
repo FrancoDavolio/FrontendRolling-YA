7c534654c1ddf5f45830f033a66c7863898205fd
Camila Loydi <camilaloydi@hotmail.com>
2022-11-27 19:36:38 -0300

    agrego componentes basico

--- src/components/views/Pedidos.jsx
+++ src/components/views/Pedidos.jsx
@@ -2,7 +2,30 @@ import React from 'react';
 
 const Pedidos = () => {
     return (
-     
+        <Container>
+        <div className="text-center">
+          <h1 className="text-light  fw-bold">Carrito Rolling-YA!</h1>
+        </div>
+
+        <Row xs={12} md={6} lg={3}>
+          {productos.map((producto) => (
+            <CardProducto
+              key={producto._id}
+              producto={producto}
+              setProductos={setProductos}
+            ></CardProducto>
+          ))}
+        </Row>
+        <hr />
+        <Row>
+            <Div>
+                <h1>TOTAL</h1>
+                <button>Cancelar</button>
+                <button>Pedir</button>
+
+            </Div>
+        </Row>
+      </Container>
     );
 };
 
